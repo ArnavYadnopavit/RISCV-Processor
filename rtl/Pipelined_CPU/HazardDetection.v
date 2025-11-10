f9e79f345fb12dd475532d8d1e6a9fa2d91b66e8
ArnavYadnopavit <yadnopavitarnav@gmail.com>
2025-11-11 00:11:00 +0530

    Branch_D

--- rtl/Pipelined_CPU/HazardDetection.v
+++ rtl/Pipelined_CPU/HazardDetection.v
@@ -32,12 +32,12 @@ module HazardDetection(
         input  wire[4:0] rd_E,
         input  wire[4:0] rd_M,
         input  wire[4:0] rd_W,
-        input  wire[1:0] PCSrc_E,
+        input  wire      PCSrc_E,
         input            regwrite_M,
         input            regwrite_W,
         input            MemtoregE,
         output reg       StallD,
-        output reg       FlushD,
+        //output reg       FlushD,
         output reg       FlushE,
         output reg[1:0]  ForwardAE,
         output reg[1:0]  ForwardBE,
@@ -49,7 +49,7 @@ module HazardDetection(
         StallD=1'b0;
         StallF=1'b0;
         FlushE=1'b0;
-        FlushD=1'b0;
+        //FlushD=1'b0;
         ForwardAE=2'b00;
         ForwardBE=2'b00;
         if(MemtoregE && (rd_E!=5'b0) && (rd_E== rs1_D||rd_E==rs2_D))begin
@@ -69,11 +69,12 @@ module HazardDetection(
         else if(regwrite_W && (rd_W != 5'b0) &&rs2_E==rd_W)begin
             ForwardBE=2'b01;  //Forward Mem result
         end
-        
-        if(PCSrc_E==1)begin
+        /*
+        if(PCSrc_E==1'b1)begin
             FlushD=1'b1;
-            FlushE=1'b1;
+            //FlushE=1'b1;
         end
+        */
     end
     
 endmodule
